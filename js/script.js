047198d912df03d98845af59fa435f704d3fc98b
rashid omarov <art89674010881@mail.ru>
2023-02-21 10:49:38 +0300

    керекр

---
+++ js/script.js
@@ -0,0 +1,72 @@
+
+// var modal = document.getElementById("myModal");
+// var btn = document.getElementById("myBtn");
+// var span = document.getElementsByClassName("close");
+
+// // btn.onclick = function(){
+// //     modal.style.display = "block";
+// // }
+// // span.onclick = function(){
+// //     modal.style.display = "none";
+// // }
+// function show(){
+//     var btn = document.getElementById("myBtn");
+//     modal.style.display = "block";
+//      }
+// function hide(){
+//     var span = document.getElementsByClassName("close");
+//     modal.style.display = "none";
+//      }
+
+
+
+//      function f1 (){
+//         const ch = document.getElementById('one');
+//      if(ch.checked){
+//      document.getElementById('te').innerHTML="есть нажатие";
+     
+//      } else {
+//         document.getElementById('te').innerHTML="нет нажатие";
+//      }
+//     }
+
+//     // кнопка бетмен
+//     function betmeen(){
+//         document.getElementById('betman').innerHTML = ('Бетмен')
+//     }
+
+//     // кнопка человека паука через события
+//     const link = document.querySelector('#g');
+//     link.addEventListener("click", function (event){
+//       document.querySelector('#spider').innerHTML=('человек паук');
+//     })
+   
+//     // блоки при клике пишут блок в консоле
+// const block1 = document.querySelector('.block1');
+// const block2 = document.querySelector('.block2');
+
+// block1.addEventListener("click",function(event){
+// console.log("Первый блок")
+// });
+// block2.addEventListener("click",function(event){
+// console.log("второй блок")
+// });
+// // выводит инфу и инпута
+// function but()  {
+// let g = document.getElementById('in').value;
+// document.getElementById('in').value = "";
+// document.getElementById('t').innerHTML = g;
+
+// }
+// function butu()  {
+//  document.getElementById('t').innerHTML = "";
+
+// }
+
+
+//         // менят цвет блока
+//         function redblue() {
+//             const red = document.getElementById('red');
+//             red.classList.toggle('block__blue');
+//          }
+ 
