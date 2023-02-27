7fa0cbe649860d106e0a7c298eacc87f4893bc94
rashid omarov <art89674010881@mail.ru>
2023-02-27 20:21:48 +0300

    trrt

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
 function plus(){
     var num1, num2,result;
-    num1 = document.getElementById('n1').ariaValueMax;
+    num1 = document.getElementById('n1').value;
     num1 = parseInt(num1);
-    num2 = document.getElementById('n2').ariaValueMax;
-    num2 = parseInt9num
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+    result = num1 + num2;
+    document.getElementById('out').innerHTML = result;
+}
+function minus(){
+    var num1, num2,result;
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+    result = num1 - num2;
+    document.getElementById('out').innerHTML = result;
+}
+function multiply(){
+    var num1, num2,result;
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+    result = num1 * num2;
+    document.getElementById('out').innerHTML = result;
 }
 
 // ПОИСКОВИК
