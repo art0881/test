3b3a6255cda5fbfe8f6dbb9b25d4a158193b118a
Omarov Rashid <art89674010881@mail.ru>
2023-02-27 19:05:00 +0300

    efwef

--- js/script.js
+++ js/script.js
@@ -1,32 +1,33 @@
+function plus(){
+    var num1, num2,result;
+    num1 = document.getElementById('n1').ariaValueMax;
+    num1 = parseInt(num1);
+    num2 = document.getElementById('n2').ariaValueMax;
+    num2 = parseInt9num
+}
 
-var slideIndex = 1;
-showSlides(slideIndex);
+// ПОИСКОВИК
+// function myFunction(){
+//     var input,filter,ul,li,p,a,i;
+//     input = document.getElementById('myInput');
+//     filter = input.value.toUpperCase();
+//     ul = document.getElementById("myUl");
+//     li = ul.getElementsByTagName('li');
+//     p = document.getElementsByTagName('p');
 
-function plusSlides(n){
-    showSlides(slideIndex += n);
-}
-function currentSlide(n){
-    showSlides(slideIndex += n);
-}
-function showSlides(n){
-    var i;
-    var slides = document.getElementsByClassName('mySlides');
-    var dots = document.getElementsByClassName('dot');
-    if(n>slides.length){
-        slideIndex = 1
-    }
-    if (n>1){
-        slideIndex=slides.length
-    }
-    for(i=0; i < slides.length;i++){
-        slides[i].style.display="none";
-    }
-    for(i=0;i<dots.length;i++){
-        dots[i].className=dots[i].className.replace("active","");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className+="active";
-}
+//     for(i=0;i<li.length;i++){
+//         a = li[i].getElementsByTagName('a')[0];
+//         if (a.innerHTML.toUpperCase().indexOf(filter)>-1){
+//             li[i].style.display = "";
+            
+//         } else {
+//             li[i].style.display = "none";
+            
+//         }
+//     }
+// }
+
+
 // var tab;
 // var tabContent;
 
