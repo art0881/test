78484d18d4ee7782ff44fc8743302fb69321e3ee
Omarov Rashid <art89674010881@mail.ru>
2023-02-28 21:58:34 +0300

    УАК

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+
+
+// ОТКРЫВАЮЩИЕ БЛОКИ
+
+// var acc = document.getElementsByClassName('accordion');
+// var i;
+// for (i =0;i <acc.length; i++){
+//     acc[i].onclick = function(){
+//         this.classList.toggle("active");
+//         this.nextElementSibling.classList.toggle("show");
+//     }
+// }
+
 // МОДАЛЬНОЕ ИЗОБРОЖЕНИЕ
 
 // var modal = document.getElementById('myModal');
