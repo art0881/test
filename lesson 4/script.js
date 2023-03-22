bdee86921e3c4a1529c22188a23dd78744250153
Omarov Rashid <art89674010881@mail.ru>
2023-03-22 04:06:26 +0300

    ЕИК

--- lesson 4/script.js
+++ lesson 4/script.js
@@ -1,36 +1,51 @@
-// const input = document.createElement('input');
-// const ul = document.createElement('ul');
+// ТУ ДО ПРИЛОЖЕНИЕ
 
-// document.body.append(input,ul);
-// input.addEventListener("keydown", keyHand);
+const input = document.createElement('input');
+const ul = document.createElement('ul');
 
-// function keyHand(even){
-//     const {key} = event;
-//     const {value} = input;
-//     if(key == "Enter" && value){
-//         const li = document.createElement("li");
-//         li.textContent = value;
-//         input.value = "";
-//         ul.append(li);
-//     }
-// }
- const button = document.querySelector('#button');
- const content = document.querySelector('#content');
+document.body.append(input,ul);
+input.addEventListener("keydown", keyHand);
 
- button.addEventListener('click', function(){
-content.classList.toggle('content-hidden');
-if(content.classList.contains('content-hidden')){
-    button.textContent = 'Открыть блок';
-} else {
-    button.textContent = 'Закрыть блок';
-}
+function keyHand(even){
+    const {key} = event;
+    const {value} = input;
+    if(key == "Enter" && value){
+        const li = document.createElement("li");
+        li.textContent = value;
+        input.value = "";
+        ul.append(li);
+    }
+};
+
+// ТАБ ОТКРЫТЬ ЗАКРЫТЬ
+
+const button = document.querySelector("#button");
+const content = document.querySelector("#content");
+
+button.addEventListener("click", function () {
+  content.classList.toggle("content-hidden");
+  if (content.classList.contains("content-hidden")) {
+    button.textContent = "Открыть блок";
+  } else {
+    button.textContent = "Закрыть блок";
+  }
+});
+
+// ПЕРЕБОР МАССИВА С ВЫВОДОМ
+
+ const array = ['привет','как дела'];
+
+const p = document.querySelector('#p');
+ array.forEach((el,i)=>{
+    p.textContent=el;
  });
 
-//  const array = ['привет','как дела'];
+// ТАБЫ МНОЖЕСТВЕННЫЕ 
 
-// const p = document.querySelector('#p');
-//  array.forEach((el,i){
-//     p.textContent=el;
-//  });
+const headers = document.querySelectorAll('[data-name="accordeon-title"]');
 
-document.querySelectorAll('[data-name="accordeon"]')
+headers.forEach(function (item) {
+  item.addEventListener("click", function () {
+    this.nextElementSibling.classList.toggle("hidden");
+  });
+});
